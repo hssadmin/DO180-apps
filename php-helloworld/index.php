d49cb1cd40e210c5cff92e2f3df2fcea4a8c5b8c
Zeng Yueming  (NCS) <yueming.zeng@ncs.com.sg>
2022-10-19 22:34:47 +0800

    mod 20221019

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -4,3 +4,13 @@ print "A console change is in the air!\n";
 print "hi hi 2021 10 18\n"
 print "Hi 2022 10 19 \n"
 ?>
+<html>
+<title>
+Hi
+</title>
+<body>
+<h1>
+HI 2022 Oct 19, have a nice day !
+</h1>
+</body>
+</html>
